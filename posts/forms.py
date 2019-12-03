a74616e16c0a812a11401ef6b873f9aa715dce2c
Alauddin Saikat <alauddinsaikat71@gmail.com>
2019-12-03 18:16:47 +0600

    form edit
    form for comment 7 create post

--- posts/forms.py
+++ posts/forms.py
@@ -6,10 +6,25 @@ class CreatePost(forms.ModelForm):
     class Meta:
         model = Posts
         fields = "__all__"
-        exclude = ('author', 'created_at',)
+        exclude = ('author', 'created_at','likes')
 
+        widgets = {
+
+            'title' : forms.TextInput(attrs={'class':'form-control'}),
+            'slug' : forms.TextInput(attrs={'class':'form-control'}),
+            'body' : forms.Textarea(attrs={'class':'form-control'}),
+        }
+        
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('text',)
+        labels = {
+        'text': ''
+    }
+        widgets = {
+
+            'text'  : forms.Textarea(attrs={'class':'form-control', 'placeholder':'Place Your Comment Here',}) 
+        }
+
