f1f1784db83d7a3c410e85c25c8d47053364b925
Alauddin Saikat <alauddinsaikat71@gmail.com>
2019-12-10 06:22:51 +0600

    trending post
    trending according to creation time

--- posts/views.py
+++ posts/views.py
@@ -19,6 +19,7 @@ def index(request):
 
     posts = Posts.objects.order_by('-created_at')
     postd = Posts.objects.order_by('-created_at')
+    
     if request.method =="POST":
         query = request.POST['q']
         if query:
@@ -33,6 +34,9 @@ def index(request):
     first_post = posts.all().order_by('-created_at')[:1]
     first_post = list(first_post)
 
+    
+    trend_post = posts.all().order_by('-created_at')[:3]
+    trend_post = list(trend_post)
     # print(first_post)
     
 
@@ -44,6 +48,7 @@ def index(request):
         'posts': posts,
         'postd':postd,
         'first_post': first_post,
+        'trend_post':trend_post,
     }
 
     return render(request, 'posts/index.html', context)
