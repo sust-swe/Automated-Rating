bc92fa8ac9bfad644bb06c37b952f02f33f47fc6
tamim36 <tamim36@student.sust.edu>
2019-12-10 19:02:30 +0600

    shell code to change rating

--- Natural Language Processing NLP/codetorun.py
+++ Natural Language Processing NLP/codetorun.py
@@ -5,6 +5,9 @@
 # filename = 'C:/Users/tamim/PyProjects/AutomatedRating/Natural Language Processing NLP/sentiment.sav'
 
 import pickle
+from posts.models import Posts
+from statistics import mean
+import numpy as np
 
 filename = 'C:/Users/tamim/PyProjects/AutomatedRating/Natural Language Processing NLP/cvectorize.sav'
 loaded_vect = pickle.load(open(filename, 'rb'))
@@ -13,3 +16,15 @@ loaded_model = pickle.load(open(filename, 'rb'))
 
 #checking
 loaded_model.predict(loaded_vect.transform(['this is super good', 'this is good']))
+
+for post in posts:
+     comments = post.comments.all()
+     rat_lst = []
+     for comment in comments:
+             #print(comment)
+             ar = loaded_model.predict(loaded_vect.transform([str(comment)]))
+             rat = (ar[0] + 1)*2
+             rat_lst.append(np.float64(rat).item())
+     rat_lst
+     post.post_item.rating = mean(rat_lst)
+     post.post_item.save()
