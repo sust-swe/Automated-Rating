9d93c80eabd9f7ec09c0dfa89a2d7796c67ded9a
tamim36 <tamim36@student.sust.edu>
2019-12-15 04:03:00 +0600

    automatically change rating

--- Natural Language Processing NLP/codetorun.py
+++ Natural Language Processing NLP/codetorun.py
@@ -12,6 +12,7 @@ import pickle
 from posts.models import Posts
 from statistics import mean
 import numpy as np
+import time
 
 filename = 'C:/Users/tamim/PyProjects/AutomatedRating/Natural Language Processing NLP/cvectorize.sav'
 loaded_vect = pickle.load(open(filename, 'rb'))
@@ -22,15 +23,17 @@ loaded_model = pickle.load(open(filename, 'rb'))
 loaded_model.predict(loaded_vect.transform(['this is super good', 'this is good']))
 
 posts = Posts.objects.all()
-for post in posts:
-     comments = post.comments.all()
-     rat_lst = []
-     for comment in comments:
-             #print(comment)
+starttime=time.time()
+while True:
+     for post in posts:
+         comments = post.comments.all()
+         rat_lst = []
+         for comment in comments:
              ar = loaded_model.predict(loaded_vect.transform([str(comment)]))
              rat = (ar[0] + 1)*2
              rat_lst.append(np.float64(rat).item())
-     rat_lst
-     if (rat_lst != []):
-         post.post_item.rating = mean(rat_lst)
-         post.post_item.save()
+         if (rat_lst != []):
+             post.post_item.rating = mean(rat_lst)
+             post.post_item.save()
+     print('success')
+     time.sleep(3.0 - ((time.time() - starttime) % 3.0))
